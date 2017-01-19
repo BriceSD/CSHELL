0ed7965f59c45bac68f28c6b75d2cc865e583201
Nicolas Brandstett <nicolas.brandstett@telecomnancy.eu>
2017-01-19 10:10:06 +0100

    Re

--- C-TP4_TP5/sources/TP4/dice_poker.c
+++ C-TP4_TP5/sources/TP4/dice_poker.c
@@ -7,10 +7,37 @@ int main() {
    /* initialise le generateur pseudo-aleatoire */
    srand(time(NULL));
    
-   /* tire un nombre au hasard entre 1 et 6 et l'affiche */
-   res = rand() % 6 + 1;
-   printf("Tirage aleatoire: %d\n", res);
+   manche();
    
    /* Programme fini avec succes */
    return 0;
+}
+
+int manche()
+{
+	int j1[5];
+	int j2[5];
+	int i=0;
+	for(i;i<4;i++)
+	{
+		j1[i] = rand() % 6 + 1;
+		j2[i] = rand() % 6 + 1;
+	}
+
+	char j1_name[4] = {'m','o','i','\0'};
+	char j2_name[4] = {'t','o','i','\0'};
+	
+	afficher(j1_name,j1);
+	afficher(j2_name,j2);
+}
+
+void afficher(char *name, int hand[5])
+{
+	printf("%s : ",name);
+	int i=0;
+	for(i;i<4;i++)
+	{
+		printf("%d ",hand[i]);
+	}
+	printf("\n");
 }
